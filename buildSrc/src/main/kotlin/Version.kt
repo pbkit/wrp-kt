8a04af23ed90035eee0d632e35b009532e37c80a
Jin Kyu Gwak <jinkyu.gwak@riiid.co>
2022-06-30 17:12:37 +0900

    version up

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -1,5 +1,5 @@
 object Version {
-    const val library_core = "0.1.1"
+    const val library_core = "0.1.2"
     const val library_generator = "0.1.1"
     const val library_webview = "0.1.2"
     const val library_webview_compose = "0.1.2"
